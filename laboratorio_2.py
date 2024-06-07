469301e7eaf7610e52c4fe3bc3ffde0e967aba70
Kenneth Perez <169111251+kennethdpp@users.noreply.github.com>
2024-06-06 22:04:31 -0400

    Add files via upload

---
+++ laboratorio_2.py
@@ -0,0 +1,16 @@
+N=input('ingrese la cantidad de numeros primos que desee, esbroba "stop" para dejar de emular numeros: ')
+L=0
+while N!='stop':
+    while not N.isnumeric():
+        print('no es un valor numerico, intente de nuevo')
+        N=input('ingrese un numero primo: ')
+        es_primo=True
+        for i in range(2,N):
+            if N%i==0:
+                es_primo=False
+                N=input('no es un numero primo, inrese un numero primo: ')
+    K=int(N)
+    C=K*K
+    break
+L=L+C
+print(L)
